e308404099445805dc339c23591cbc1bfb889ab1
hex <hex@hex.com>
2016-01-18 23:13:28 +0800

    Data not continuous

--- Client/client.cpp
+++ Client/client.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 #ifdef PLATFORM_IS_LINUX
-    Robots::SendRequest(argc, argv, "/home/hex/Desktop/RobotGaits/resource/Robot_III/Robot_VIII.xml");
+    Robots::SendRequest(argc, argv, "/home/hex/Desktop/RobotGaits/resource/Robot_III/Robot_III.xml");
 #endif
 #ifdef PLATFORM_IS_WINDOWS
 	Robots::SendRequest(argc, argv, "C:\\Robots\\resource\\Robot_Type_I\\Robot_VIII\\Robot_VIII.xml");
